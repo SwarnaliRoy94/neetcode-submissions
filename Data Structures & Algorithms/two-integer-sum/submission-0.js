e9a67573191c780218bdf594d6450a7db27a0a9a
Swarnali Roy <76678123+SwarnaliRoy94@users.noreply.github.com>
2026-04-30 20:50:25 +0600

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const seen = {};
+        for (let i=0 ; i< nums.length; i++){
+            let difference = target - nums[i];
+            if(difference in seen) return [seen[difference],i]
+            seen[nums[i]]=i;
+        }
+    }
+}
